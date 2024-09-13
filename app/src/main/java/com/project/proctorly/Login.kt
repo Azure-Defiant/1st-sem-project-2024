154164ff16913a0c642458e9db045f7e5eab0362
Pixel Pulse <aquinosherwinjosh@gmail.com>
2024-09-13 18:41:29 +0800

    added login activity

--- app/src/main/java/com/project/proctorly/Login.kt
+++ app/src/main/java/com/project/proctorly/Login.kt
@@ -1,26 +1,20 @@
 package com.project.proctorly
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class GetStarted : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.get_started)
-
-        val getstartedbutton = findViewById<Button>(R.id.btngetstarted)
-
-        getstartedbutton.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-
     }
 }
