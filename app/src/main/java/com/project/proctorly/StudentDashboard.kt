d9e8a7099b9c70fa16caf068dd53ad3ab3747330
Pixel Pulse <aquinosherwinjosh@gmail.com>
2024-09-16 14:06:00 +0800

    navigation menu in student dashboard

---
+++ app/src/main/java/com/project/proctorly/StudentDashboard.kt
@@ -0,0 +1,22 @@
+package com.project.proctorly
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class StudentDashboard : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_student_dashboard)
+
+
+    }
+}
